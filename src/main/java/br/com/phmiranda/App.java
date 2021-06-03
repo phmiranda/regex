e43d89c00f803e84a1e1850493febb6bf0acdcd3
phmiranda <pehhagah.1607@gmail.com>
2021-06-03 11:36:38 -0300

    [create] - criação do exemplo de PATTERN utilizando JAVA.

--- src/main/java/br/com/phmiranda/App.java
+++ src/main/java/br/com/phmiranda/App.java
@@ -1,13 +1,34 @@
 package br.com.phmiranda;
 
+import br.com.phmiranda.util.ValidateBaseUtil;
+import br.com.phmiranda.validate.*;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        ValidateBaseUtil validateBaseUtil = new ValidateBaseUtil();
+        ValidacaoNomeCompletoUsuario validaNomeUsuario = new ValidacaoNomeCompletoUsuario();
+        ValidacaoTelefoneCelular validacaoTelefoneCelular = new ValidacaoTelefoneCelular();
+        ValidacaoTelefoneFixo validacaoTelefoneFixo = new ValidacaoTelefoneFixo();
+        ValidacaoDocumentoPessoal validacaoDocumentoPessoal = new ValidacaoDocumentoPessoal();
+        ValidacaoData validacaoData = new ValidacaoData();
+
+        validateBaseUtil.validate("11a22b33c");
+        validaNomeUsuario.validaNomePessoal();
+        validacaoTelefoneCelular.validaTelefoneCelular();
+        validacaoTelefoneFixo.validaTelefoneFixo();
+        validacaoDocumentoPessoal.validaDocumentoPessoal();
+        validacaoData.validaDataDeNascimento();
+
+
+        System.out.println("RESULTADO: " + validateBaseUtil);
+        System.out.println("VALIDAÇÃO DO NOME: " + validaNomeUsuario);
+        System.out.println("VALIDAÇÃO DO DOCUMENTO: " + validacaoDocumentoPessoal);
+        System.out.println("VALIDAÇÃO DO NÚMERO FIXO: " + validacaoTelefoneFixo);
+        System.out.println("VALIDAÇÃO DO NÚMERO DE CELULAR: " + validacaoTelefoneCelular);
+        System.out.println("VALIDAÇÃO DO DATA DE NASICMENTO: " + validacaoData);
     }
 }
---
+++ src/main/java/br/com/phmiranda/validate/ValidacaoDocumentoPessoal.java
@@ -0,0 +1,14 @@
+/**
+ * User: Pedro
+ * Project: regex
+ * Description: N/A
+ * Date: 02/06/2021
+ */
+
+package br.com.phmiranda.validate;
+
+public class ValidacaoDocumentoPessoal {
+    public void validaDocumentoPessoal(){
+
+    }
+}
